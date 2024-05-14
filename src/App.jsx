eea03bdd7e5cdc38df44edd3ec8478930ac3b253
johnbean36 <jmbass36@gmail.com>
2024-05-13 23:15:11 -0500

    finished for the night

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,48 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from '../components/Nav'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [user, setUser] = useState(null)
+  const [userName, setUserName] = useState(null);
+  const [userEmail, setUserEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  function handleSubmit(e){
+    
+  }
+
+  function handleChange(e, input){
+    if(input === "name"){
+      setUserName(e);
+    }
+    else if(input==="email"){
+      setUserEmail(e);
+    }
+    else if(input==="password"){
+      setPassword(e);
+    }
+  }
 
   return (
     <div>
       <header>
         <Nav user={user} />
       </header>
+      <main>
+        <Routes>
+          <Route path='/signup' element={ <Signup setUserName={setUserName}
+                                           setUserEmail={setUserEmail} 
+                                           setPassword={setPassword}
+                                           userName={userName}
+                                           userEmail={userEmail}
+                                           password={password}
+                                           handleSubmit={handleSubmit}
+                                           handleChange={handleChange} /> } />
+                                           
+        </Routes>
+      </main>
     </div>
   )
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,11 @@
+function Signup({setUserName, setUserEmail, setPassword, userName}){
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <div><label for="name">Enter your name: </label></div>
+            <div><input id="name" value={} /></div>
+        </div>
+    )   
+}
+
+export default Signup;
