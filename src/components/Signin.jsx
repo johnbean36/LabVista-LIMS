e1c888c4686e1170b3461caa392d039b4892aee7
johnbean36 <jmbass36@gmail.com>
2024-05-21 09:10:31 -0500

    Finished login page bootstrap

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,18 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form, Button} from 'react-bootstrap';
+import {Form, Button, Row, Col} from 'react-bootstrap';
 
 function Signin({userEmail, handleChange, password, handleSubmit, error}){
     return (
         <div>
-            <div>
+            <div className="margin select">
                 <Form onSubmit={(e)=>(handleSubmit(e, "signin"))} >
-                    <Form.Group>
-                        <div><Form.Label htmlFor="email">Enter your email: </Form.Label></div>
-                        <div><Form.Control type="email" id="email" value={userEmail} onChange={(e)=>(handleChange(e, "email"))} /></div>
+                    <Form.Group as={Row} className="mb-3 justify-content-center">
+                        <div><Form.Label column sm="5" htmlFor="email">Enter your email: </Form.Label></div>
+                        <Col sm="10">
+                            <div><Form.Control type="email" id="email" value={userEmail} onChange={(e)=>(handleChange(e, "email"))} /></div>
+                        </Col>
                     </Form.Group>
-                    <Form.Group>
-                        <div><Form.Label htmlFor="password">Enter a password: </Form.Label></div>
-                        <div><Form.Control type="password" id="password" value={password} onChange={(e)=>(handleChange(e, "password"))} /></div>
+                    <Form.Group as={Row} className="mb-3 justify-content-center">
+                        <div><Form.Label column sm="5" htmlFor="password">Enter a password: </Form.Label></div>
+                        <Col sm="10">
+                            <div><Form.Control type="password" id="password" value={password} onChange={(e)=>(handleChange(e, "password"))} /></div>
+                        </Col>
                     </Form.Group>
                     <Form.Group>
                         <Button type="submit">Submit</Button>
